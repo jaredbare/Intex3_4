cd6cfde7c8d386e430e11cb0ac4341bf17549fd0
jaredbare <jab468@byu.edu>
2023-02-28 16:41:52 -0700

    Add project files.

---
+++ Mission09_jab468/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission09_jab468.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
