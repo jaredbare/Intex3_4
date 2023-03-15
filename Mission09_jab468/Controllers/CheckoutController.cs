a83cbf797fc8fe2efc76f6e4e3d8ae4963886b3f
jaredbare <jab468@byu.edu>
2023-03-15 15:09:43 -0600

    11.4

---
+++ Mission09_jab468/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_jab468.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jab468.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Checkout());
+        }
+    }
+}
