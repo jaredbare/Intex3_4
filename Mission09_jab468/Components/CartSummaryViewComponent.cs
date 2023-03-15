ce36987d1759f70205ab3c829eed5500641cafa6
jaredbare <jab468@byu.edu>
2023-03-15 17:24:26 -0600

    cart icon

---
+++ Mission09_jab468/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission09_jab468.Models;
+
+namespace Mission09_jab468.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
