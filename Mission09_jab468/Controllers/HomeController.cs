a5d68569e1452f9d3a00ec9348e640f7eda58666
jaredbare <jab468@byu.edu>
2023-04-10 16:45:36 -0600

    added view

--- Mission09_jab468/Controllers/HomeController.cs
+++ Mission09_jab468/Controllers/HomeController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Intex3_4.Controllers
 {
@@ -20,14 +19,6 @@ namespace Intex3_4.Controllers
         //    _logger = logger;
         //}
         private IBookListRepository repo;
-
-        //private BookstoreContext mummyContext { get; set; }
-
-        //Constructor
-        //public HomeController(BookstoreContext artifact)
-        //{
-        //    mummyContext = artifact;
-        //}
 
         public HomeController(IBookListRepository temp)
         {
@@ -71,48 +62,20 @@ namespace Intex3_4.Controllers
             //    .Take(pageSize);
             return View(x);
         }
+
         public IActionResult Privacy()
         {
             return View();
         }
-
         public IActionResult Background()
         {
             return View();
         }
-        //[HttpGet]
-        //public IActionResult Edit(int BurialId)
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
         //{
-        // there's no need to create something totally new here
-        // so instead we send it back to our 'list of options'
-        //COME BACK HERE AND CHANGE THE NAMES
-        //ViewBag.Categories = mummyContext.Categories.ToList();
-        //var burial = mummyContext.Responses.Single(x => x.BurialId == BurialId);
-        // we return to the view (our application) so that we can see and make changes
-        // we pass in the var we just created
-        //return View("MovieForm", burial);
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         //}
-
-        //[HttpPost]
-        // receives an instance of an application response that we can refer to
-        //        public IActionResult Edit(Response egypt)
-        //        {
-        //            here we update and save changes based on the info passed above
-        //            mummyContext.Update(egypt);
-        //            mummyContext.SaveChanges();
-
-        //            we pass it all into the waitlist cshtml
-        //             remember that you must also specify an action that will
-        //             actually go back to the previous waitlist action we made so it
-        //             not only shows the view but also pulls in the actions
-        //            return RedirectToAction("Waitlist");
-
-        //        }
-
-        //        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //        public IActionResult Error()
-        //        {
-        //            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        //        }
     }
 }
