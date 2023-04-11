fe671cf23801f2434cf2447ba63d53a3d7413e28
nati2024 <95377889+nati2024@users.noreply.github.com>
2023-04-11 12:44:46 -0600

    some stuff

--- Mission09_jab468/Models/Response.cs
+++ Mission09_jab468/Models/Response.cs
@@ -13,21 +13,19 @@ namespace Intex3_4.Models
         public int BurialId { get; set; }
 
         [Required]
-        public string Title { get; set; }
-        [Required]
         public string Color { get; set; }
         [Required]
         public string Structure { get; set; }
         [Required]
         public string Sex { get; set; }
         [Required]
-        public string Depth { get; set; }
+        public int Depth { get; set; }
         [Required]
-        public string Stature { get; set; }
+        public int Stature { get; set; }
         [Required]
-        public string Age { get; set; }
+        public int Age { get; set; }
         [Required]
-        public string Head { get; set; }
+        public int Head { get; set; }
         [Required]
         public string Function { get; set; }
         [Required]
