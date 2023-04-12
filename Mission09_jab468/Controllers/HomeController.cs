81ff8fc889f6b2c08a94421ad76a1981e88dd9cb
jaredbare <jab468@byu.edu>
2023-04-12 11:44:53 -0600

    pagination

--- Mission09_jab468/Controllers/HomeController.cs
+++ Mission09_jab468/Controllers/HomeController.cs
@@ -53,19 +53,28 @@ namespace Intex3_4.Controllers
             return View();
         }
 
-        public ActionResult Index2(int recordCount = 20)
+        public IActionResult Index2(int recordCount=10,int page=1)
 
         {
-            using (var context = new MummiesContext()) // Replace YourDbContext with the actual name of your DbContext class
-            {
-                // Get data from Textile table
-                var textiles = context.Textile.Take(recordCount).ToList(); // Retrieve Textile data from the database and convert to a List
+            //int defaultRecordCount = 10;
+            //int recordCount = defaultRecordCount;
+            //if (Request.Form["recordCount"] != null)
+            //{
+            //    int.TryParse(Request.Form["recordCount"], out recordCount);
+            //}
+            //else if (Session["recordCount"]) != null){
+            //    recordCount = (int)Session["recordCount"];
+            //}
+            //Session["recordCount"] = recordCount;
 
-                // Get data from Burialmain table
-                var burials = context.Burialmain.Take(recordCount).ToList(); // Call your existing method to retrieve Burialmain data
-                var structure = context.Structure.Take(recordCount).ToList();
-                var color = context.Color.Take(recordCount).ToList();
-                var function = context.Textilefunction.Take(recordCount).ToList();
+            using (var context = new MummiesContext()) 
+            {
+                int skipCount = (page - 1) * recordCount;
+                var textiles = context.Textile.Skip(skipCount).Take(recordCount).ToList(); 
+                var burials = context.Burialmain.Skip(skipCount).Take(recordCount).ToList(); 
+                var structure = context.Structure.Skip(skipCount).Take(recordCount).ToList();
+                var color = context.Color.Skip(skipCount).Take(recordCount).ToList();
+                var function = context.Textilefunction.Skip(skipCount).Take(recordCount).ToList();
 
                 // Create and populate view model
                 var viewModel = new BodyViewModel()
@@ -76,7 +85,15 @@ namespace Intex3_4.Controllers
                     Color = color,
                     Function = function
                 };
+                // Calculate total number of records
+                int totalRecords = context.Burialmain.Count(); // assuming Textile table is used as the base for pagination
 
+                // Calculate total number of pages
+                int totalPages = (int)Math.Ceiling((double)totalRecords / recordCount);
+
+                // Pass pagination information to view
+                ViewBag.TotalPages = totalPages;
+                ViewBag.CurrentPage = page;
                 return View(viewModel);
             }
         }
